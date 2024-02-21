29f401de0570cced3e93f58a3cfc7c6294207f26
shubham yadav <48561090+shubhamya@users.noreply.github.com>
2024-02-21 15:43:37 +0000

    variables and datatypes

---
+++ basics of JS/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = "sy412726@gmail.com"
+accountPassword = "12345"
+accountCity = "Gurugram"
+accountEmail = "sy1737935@gmail.com"
+accountPassword = "2345"
+accountCity = "Indore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ basics of JS/02_datatypes.js
@@ -0,0 +1,12 @@
+// number = 2 to power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined = no value assigned
+// symbol = unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
