5c43ba6c7bd461f1426042593fc0194918cc23c3
shubham yadav <48561090+shubhamya@users.noreply.github.com>
2024-02-23 21:46:58 +0000

    datatype summary

--- basics of JS/datatypes-summary.js
+++ basics of JS/datatypes-summary.js
@@ -1,11 +1,11 @@
-//primitive 
+// //primitive 
 
-// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+// // 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
 
 const score = 100
 const scoreValue = 100.3
 const isLoggedIn = false
-const outsideTemp = null (tyoeof value of null is object)
+const outsideTemp = null (typeof value of null is object)
 let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
@@ -23,8 +23,31 @@ let myObj = {
     age: 26,
 }
 
-const myFunction = function(){
+const myFunction = function();
 console.log("hello World");
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++
+
+// stack (primitive), Heap (Non-Primitive)
+
+let myYoutubename = "Shubhamyadavdotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaursutta"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    age: "26"
 }
 
+let userTwo = userOne
 
+userTwo.email = "shubham@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
