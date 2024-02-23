ce805d9779b660b4b1bd576d7ee6a971121d98a3
shubham yadav <48561090+shubhamya@users.noreply.github.com>
2024-02-23 21:04:28 +0000

    datatype summary

---
+++ basics of JS/datatypes-summary.js
@@ -0,0 +1,30 @@
+//primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null (tyoeof value of null is object)
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+const bigNumber = 232452346376n (not used much)
+
+
+
+//Reference Type (Non Primitive)
+
+// Array, Objects, functions (type of value of non primitives are object)
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "shubham",
+    age: 26,
+}
+
+const myFunction = function(){
+console.log("hello World");
+}
+
+
