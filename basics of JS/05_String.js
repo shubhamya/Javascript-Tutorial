1231c9d1d92833f4770af82425ab7b22be79d71b
shubham yadav <48561090+shubhamya@users.noreply.github.com>
2024-02-24 07:09:47 +0000

    strings

---
+++ basics of JS/05_String.js
@@ -0,0 +1,12 @@
+const name = "Shubham"
+const repoCount = 50
+
+console.log(`${name.ToUppercase}, ${repoCount}`);
+
+const gameName = new String('shubham')
+
+console.log(gameName[2]);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('u'));
