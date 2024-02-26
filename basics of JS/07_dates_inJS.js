ea9ff4ef68bab376653c5a250ae055b4761fe656
shubham yadav <48561090+shubhamya@users.noreply.github.com>
2024-02-26 11:08:47 +0000

    datesinjs

---
+++ basics of JS/07_dates_inJS.js
@@ -0,0 +1,18 @@
+let myDate = new Date(2023, 4, 26, 7, 9, 9, 9)
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate); //Date is object
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDate());
+console.log(newDate.getFullYear());
