328fc80db8c646bb9175c0d84d55c32370fa5a29
shubham yadav <48561090+shubhamya@users.noreply.github.com>
2024-02-26 11:08:24 +0000

    nums and maths

--- basics of JS/06_nums_and_maths.js
+++ basics of JS/06_nums_and_maths.js
@@ -1,29 +1,29 @@
-// const score = 100
-// console.log(score);
+const score = 100
+console.log(score);
 
-// const balance = new Number(200)
-// console.log(balance);
+const balance = new Number(200)
+console.log(balance);
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(2));
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
 
-// const otherNumber = 23.555
+const otherNumber = 23.555
 
-// console.log(otherNumber.toPrecision(2));
+console.log(otherNumber.toPrecision(2));
 
-// const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN')); //'en-IN vlaue is indian value'
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN')); //'en-IN vlaue is indian value'
 
-// //+++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// console.log(Math); //Math is an object value
-// console.log(Math.abs()); //abs is absolute value
+console.log(Math); //Math is an object value
+console.log(Math.abs()); //abs is absolute value
 
-// console.log(Math.round(4.3)); //gives round value
-// console.log(Math.ceil(4.2)); //gives top value
-// console.log(Math.floor(4.9)); //gives bottom value
-// console.log(Math.min(4, 3, 5, 6)); // gives minimum value
-// console.log(Math.max(4, 3, 6, 8)); // gives max value
+console.log(Math.round(4.3)); //gives round value
+console.log(Math.ceil(4.2)); //gives top value
+console.log(Math.floor(4.9)); //gives bottom value
+console.log(Math.min(4, 3, 5, 6)); // gives minimum value
+console.log(Math.max(4, 3, 6, 8)); // gives max value
 
 console.log(Math.random()); // gives random value and gives value between 0 and 1 (decimal mostly)
 console.log((Math.random()*10) + 1); //shift value before one decimal
