7b8c2a48046d0622932b59b60d4806c7cc009aa0
shubham yadav <48561090+shubhamya@users.noreply.github.com>
2024-02-26 09:42:52 +0000

    strings

--- basics of JS/05_String.js
+++ basics of JS/05_String.js
@@ -10,3 +10,21 @@ console.log(gameName.toUpperCase());
 
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('u'));
+
+const newString = gameName.substring(0, 5);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "     Shubham     "
+console.log(newStringOne.trim());
+console.log(newStringOne);
+
+const url = "https://shubham.com/shubham%20yadav"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('shubham'));
+
+console.log(gameName.split('-'));
